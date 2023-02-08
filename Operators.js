ebb73e53265cd6a18a979bda25191ab335419582
jawad <jawad@GHOST.(none)>
2023-02-08 10:55:21 +0500

    Adding Operators.js

---
+++ Operators.js
@@ -0,0 +1,39 @@
+console.log("Operators");
+console.log("\n------------------------------------------------------------------------")
+console.log("Arithmetic Operators");
+
+a = 232
+b = 989
+console.log("Addition: ",a+b);
+console.log("Subtraction: ", a-b);
+console.log("Product: ", a*b);
+console.log("Divsion: ", a/b);
+console.log("Modulus: ", b%a);
+console.log("Increment a: ",a++);
+console.log("Decrement b: ",b--);
+
+console.log("\n------------------------------------------------------------------------")
+console.log("Assignment Operators");
+
+console.log("Adding 10 in a: ", a+=10);
+console.log("Subtracting 20 in b: ", b-=20);
+console.log("Multiplying 4 by a: ", a*=4);
+console.log("Dividing 7 by b: ",b/=7);
+console.log("Remainder in a/5: ", a%=5);
+
+console.log("\n------------------------------------------------------------------------")
+console.log("Comparison Operators");
+
+console.log("a==b", a==b);
+console.log("a===b", a===b);
+console.log("a!=b", a!=b);
+console.log("a>b", a>b);
+console.log("a<b", a<b);
+console.log("a>=b", a>=b);
+console.log("a<=b", a<=b);
+console.log("\n------------------------------------------------------------------------")
+console.log("Logical Operators");
+
+console.log("OR operation a>b||b>a: ", a>b||b>a);
+console.log("AND operation a>b&&b>a: ", a>b&&b>a);
+console.log("NOT operation: ", a=!a);
