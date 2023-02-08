35c44b15330dd0e3cfffedeeb748454510e569b8
Jawad Ali <106097383+notjawad404@users.noreply.github.com>
2023-02-08 11:13:01 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+var petDog = "Rex"
+var petCat = "Pepper"
+console.log(petDog);
+console.log(petCat)
+console.log("My pet cat's name is: "+petCat)
+console.log("My pet dog's name is: "+petDog)
+
+var catSound = "purr"
+var dogSound = "woof"
+
+console.log(petDog +" says "+ dogSound);
+console.log(petCat +" says "+catSound);
+catSound = "meow"
+console.log(petCat +" now says "+catSound)
