38217aa69a4a914d6143a70f3c9fbe604331df92
Jawad Ali <106097383+notjawad404@users.noreply.github.com>
2023-03-07 10:01:38 +0500

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,14 @@
+var object1 = {
+    name: "Alex",
+    id: 1234,
+    location: "Dallas, USA",
+    data1: function(){
+        console.log("Name: ", this.name, "\nID: ", this.id, "\nAddress: ", this.location)
+    }
+}
+console.log("Object 1 ")
+object1.data1();
+
+var object2 = {
+    
+}
